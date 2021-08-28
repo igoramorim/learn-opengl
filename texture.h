7e811aeca359aa763830918ac7aecf707148ba9b
Igor Amorim <igor340@gmail.com>
2021-08-28 17:15:05 -0300

    texture class

---
+++ texture.h
@@ -0,0 +1,102 @@
+#pragma once
+
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+#include <glad/glad.h>
+#include "stb_image.h"
+#include "shader.h"
+
+class Texture
+{
+public:
+	unsigned int ID;
+	GLenum type;
+	GLenum slot;
+
+	Texture(const char* imagePath, GLenum texType, GLenum slotType, GLenum sourceFormat, GLenum destFormat, GLenum pixelType)
+	{
+		type = texType;		// assigns the type of the texture to the texture object
+		slot = slotType;	// assigns the slot type of the texture
+
+		// stbi_set_flip_vertically_on_load(true);
+
+		// generate an OpenGL texture object
+		glGenTextures(1, &ID);
+		// glActiveTexture(slot);
+
+		// bind the texture. all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+		bind();
+
+		// set the texture wrapping parameters
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+		// set texture filtering parameters
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); // when downscaling
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); // when upscaling
+
+		// stores the width, height, and the number of color channels of the image
+		int widthImg, heightImg, nrChannels;
+
+		// load image data in bytes
+		unsigned char* imageData = stbi_load(imagePath, &widthImg, &heightImg, &nrChannels, 0);
+
+		if (imageData)
+		{
+			glTexImage2D(GL_TEXTURE_2D,	// specifies the texture target that was bind with glBindTexture(GL_TEXTURE_2D, ...) previously
+				0,						// specifies the mipmap level if you want to set manually. 0 is the base level
+				destFormat,				// the format we want OpenGL to store our texture
+				widthImg,				// width of the the texture
+				heightImg,				// height of the texture
+				0,						// dont know what it is. set 0 always
+				sourceFormat,			// format of the source image
+				pixelType,				// datatype wich the image was loaded in. we loaded the image(imageData) as chars(bytes)
+				imageData);				// image data
+
+			glGenerateMipmap(texType); // generate all mipmaps levels for the active binded texture
+		}
+		else
+		{
+			std::cout << "ERROR::TEXTURE::FAILED_TO_LOAD_TEXTURE" << std::endl;
+		}
+
+		stbi_image_free(imageData); // free image memory
+
+		// unbinds the OpenGL Texture object so that it can't accidentally be modified
+		glBindTexture(texType, 0);
+	}
+
+	void texUnit(Shader& shader, const char* uniform, GLuint unit)
+	{
+		// Shader needs to be activated before changing the value of a uniform
+		shader.use();
+		// Sets the value of the uniform
+		shader.setInt(uniform, unit);
+	}
+
+	void activeAndBind()
+	{
+		glActiveTexture(slot);
+		bind();
+	}
+
+	void bind()
+	{
+		glBindTexture(type, ID);
+	}
+
+	void unbind()
+	{
+		glBindTexture(type, 0);
+	}
+
+	void Delete()
+	{
+		glDeleteTextures(1, &ID);
+	}
+
+	// TODO: function to set the wrapping and filtering options that was set default values in constructor
+};
+
+#endif
---
+++ textures1.cpp
@@ -0,0 +1,138 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include "shader.h"
+#include "texture.h"
+#include "stb_image.h"
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	if (glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS)
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+}
+
+const unsigned int SCREEN_WIDTH = 800;
+const unsigned int SCREEN_HEIGHT = 600;
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+	wWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+	// Glfw window creation
+	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << '\n';
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// Glad load all OpenGL function pointers
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << '\n';
+		return -1;
+	}
+
+	// build and compile our shader program
+	Shader shader("shaders/texture.vs", "shaders/texture.fs");
+
+	// Vertex input data
+	float vertices[] = {
+		// positions         // colors           // texture coords
+		0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   2.0f, 2.0f,   // top right
+		0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   2.0f, 0.0f,   // bottom right
+	   -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+	   -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 2.0f    // top left
+	};
+
+	unsigned int indices[] = {
+		0, 1, 3, // first triangle
+		1, 2, 3  // second triangle
+	};
+
+	unsigned int VBO;
+	unsigned int VAO;
+	unsigned int EBO;
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	// Setup triangle
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// Position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// Color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	// Texture Coord attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+	Texture woodTex("textures/container.jpg", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGB, GL_RGB, GL_UNSIGNED_BYTE);
+	woodTex.texUnit(shader, "texture1", 0);
+
+	Texture smileTex("textures/awesomeface.png", GL_TEXTURE_2D, GL_TEXTURE1, GL_RGBA, GL_RGBA, GL_UNSIGNED_BYTE);
+	smileTex.texUnit(shader, "texture2", 1);
+
+	// Render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		processInput(window);
+
+		glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+		woodTex.activeAndBind();
+		smileTex.activeAndBind();
+
+		float time = glfwGetTime();
+		shader.use(); // render the triangle
+		shader.setFloat("time", time); // set the uniform shader variable
+
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		glfwSwapBuffers(window); // Swap the front buffer and the back buffer
+		glfwPollEvents(); // Checks if any events are triggered (like keyboard)
+	}
+
+	// De-allocate all resources once they were already used
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+	// woodTex.Delete();
+	// smileTex.Delete();
+
+	glfwTerminate();
+	return 0;
+}
