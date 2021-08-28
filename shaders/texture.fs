1c8cf0e321d2026bc471ea76d7250f0a13d9d128
Igor Amorim <igor340@gmail.com>
2021-08-28 13:51:54 -0300

    textures

--- shaders/texture.fs
+++ shaders/texture.fs
@@ -13,6 +13,7 @@ uniform sampler2D texture2;
 void main()
 {
    // FragColor = vec4(ourColor, 1.0);
-   FragColor = texture(texture2, vec2(1.0 -  TexCoord.x,  TexCoord.y)) * vec4(ourColor, 1.0);
-   // FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+   //FragColor = texture(texture2, vec2(1.0 -  TexCoord.x,  TexCoord.y)) * vec4(ourColor, 1.0);
+   FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+   // FragColor = texture(texture2, TexCoord);
 };
--- textures.cpp
+++ textures.cpp
@@ -58,10 +58,10 @@ int main()
 	// Vertex input data
 	float vertices[] = {
 		 // positions         // colors           // texture coords
-		 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
-		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+		 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   2.0f, 2.0f,   // top right
+		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   2.0f, 0.0f,   // bottom right
 		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
-		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 2.0f    // top left
 	};
 
 	unsigned int indices[] = {
@@ -104,7 +104,7 @@ int main()
 		          &texture1);	// array of int that stores the generated textures IDs
 	glBindTexture(GL_TEXTURE_2D, texture1);	// all upcoming GL_TEXTURE_2D operations now have effect on this texture object
 	// set the texture wrapping parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);	// set texture wrapping to GL_REPEAT (default wrapping method)
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	// set texture filtering parameters
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);	// when downscaling
