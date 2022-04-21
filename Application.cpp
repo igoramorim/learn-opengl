f1653ffeab17b949184677d4b0716c19025c9ca1
Igor Amorim <igor340@gmail.com>
2022-04-21 16:03:33 -0300

    Cleaning Application.cpp

--- Application.cpp
+++ Application.cpp
@@ -1,10 +1,5 @@
 #include <iostream>
 
-// #include <glad/glad.h>
-// #include <GLFW/glfw3.h>
-// #include <glm/glm.hpp>
-// #include <glm/gtc/matrix_transform.hpp>
-// #include <glm/gtc/type_ptr.hpp>
 #include <imgui/imgui.h>
 #include <imgui/imgui_impl_glfw.h>
 #include <imgui/imgui_impl_opengl3.h>
@@ -29,56 +24,8 @@ void processInput(GLFWwindow* window);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 
-// camera
-// Camera camera(glm::vec3(0.0f, 0.0f, 5.0f));
-// float lastX = Constants::LAST_X;
-// float lastY = Constants::LAST_Y;
-
-// timing
-// float deltaTime = 0.0f; // time between current frame and last frame
-// float lastFrame = 0.0f;
-
 int main()
 {
-	/*glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-#ifdef __APPLE__
-	wWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-
-	// Glfw window creation
-	GLFWwindow* window = glfwCreateWindow(Constants::SCREEN_WIDTH, Constants::SCREEN_HEIGHT, "LearnOpenGL", NULL, NULL);
-	if (window == NULL)
-	{
-		std::cout << "Failed to create GLFW window" << '\n';
-		glfwTerminate();
-		return -1;
-	}*/
-
-	// glfwMakeContextCurrent(window);
-
-	// ========================================================================
-	// TODO: Implementar os callbacks no WindowManager
-
-	// glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-	// glfwSetCursorPosCallback(window, mouse_callback);
-	// glfwSetScrollCallback(window, scroll_callback);
-	// ========================================================================
-
-	// glfwSetCursorPos(window, lastX, lastY);
-
-	// Capture the mouse cursor
-	// glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL); // GLFW_CURSOR_NORMAL - GLFW_CURSOR_DISABLED
-
-	// Glad load all OpenGL function pointers
-	/*if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cout << "Failed to initialize GLAD" << '\n';
-		return -1;
-	}*/
-
 	auto windowManager = WindowManager::instance();
 
 	IMGUI_CHECKVERSION();
@@ -105,19 +52,9 @@ int main()
 	// Render loop
 	while (!windowManager->WindowShouldClose())
 	{
-
 		windowManager->CalculateDeltaTime();
-		/*
-		float time = glfwGetTime();
-		// per-frame time logic
-		float currentFrame = time;
-		deltaTime = currentFrame - lastFrame;
-		lastFrame = currentFrame;
-		*/
 
 		windowManager->ClearScreen();
-		//glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// Init ImGui
 		ImGui_ImplOpenGL3_NewFrame();
@@ -152,9 +89,7 @@ int main()
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
 		windowManager->SwapBuffers();
-		// glfwSwapBuffers(window); // Swap the front buffer and the back buffer
 		windowManager->PollEvents();
-		// glfwPollEvents(); // Checks if any events are triggered (like keyboard)
 	}
 
 	if (currentDemo != menu)
@@ -167,27 +102,5 @@ int main()
 	ImGui::DestroyContext();
 
 	windowManager->Terminate();
-	// glfwTerminate();
 	return 0;
 }
-
-/*void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-	glViewport(0, 0, width, height);
-}*/
-
-/*void mouse_callback(GLFWwindow* window, double xpos, double ypos)
-{
-	float xoffset = xpos - lastX;
-	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
-
-	lastX = xpos;
-	lastY = ypos;
-
-	camera.processMouseMovement(xoffset, yoffset);
-}
-
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
-{
-	camera.processMouseScroll(yoffset);
-}*/
